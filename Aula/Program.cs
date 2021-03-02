9c19eaf5bd0a7c2f682073deac5f9275cc97d0eb
Ramon Fernandes <ramonmfb777@gmail.com>
2021-03-02 11:37:55 -0300

    exercicio de numeros impares

---
+++ Aula/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos números inteiro você vai digitar?");
+            int x = int.Parse(Console.ReadLine());
+            while (x > 1000 || x < 1)
+            {
+                Console.WriteLine("Escolhe um numero entre 1 e 1000");
+                x = int.Parse(Console.ReadLine());
+            }
+            for (int i = 1; i <= x; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine("--------------");
+                    Console.WriteLine("ímpar: " + i);
+                }
+            }
+            Console.WriteLine("--------------");
+        }
+    }
+            
+}
+
+        
+
+
